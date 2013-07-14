c1e9e36ceec502bb475e0d486846c5d438c5d3e1
elizabeth sall <elizabeth@gmail>
2013-07-14 05:29:50 +0000

    trying out buffers

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var indexContents = fs.readFileSync("index.html")
+
+
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
-});
+  response.send('Hello World 3!');
+  response.send(indexContents); 
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
